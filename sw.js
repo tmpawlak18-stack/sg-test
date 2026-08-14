93385a65a6eab8b16be49f6084065ccb13022e49
tmpawlak18-stack <tmpawlak18@gmail.com>
2026-08-14 22:37:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
-const CACHE = "sg-test-v1";
-const FILES = [
-  "./",
-  "./SG_test_wiedzy_200_pytan.html",
-  "./manifest.webmanifest"
-];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+const CACHE = "sg-test-v2";
+const FILES = ["./", "./index.html", "./manifest.webmanifest"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
   self.skipWaiting();
 });
-self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(r => {
-      const copy = r.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy));
-      return r;
-    }).catch(() => cached))
-  );
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  })));
 });
