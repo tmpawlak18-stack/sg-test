b0169913e95336b9d4e1d491ec5fac155845cf58
tmpawlak18-stack <tmpawlak18@gmail.com>
2026-09-08 21:55:41 +0200

    Add files via upload

--- policja/sw.js
+++ policja/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "testrekrut-policja-v3";
-const FILES = ["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png","./icon-header-policja.png","./policja-logo.png"];
+const CACHE = "testrekrut-policja-v4";
+const FILES = ["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png","./icon-header-policja.png","./policja-logo.png","./policja-bg.jpg"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener("fetch", event => { const req=event.request; if(new URL(req.url).origin!==self.location.origin) return; if(req.mode==="navigate"){ event.respondWith(fetch(req).then(r=>{const c=r.clone(); caches.open(CACHE).then(x=>x.put("./index.html",c)); return r;}).catch(()=>caches.match("./index.html"))); return; } event.respondWith(caches.match(req).then(c=>c||fetch(req).then(r=>{if(r.ok){const cp=r.clone();caches.open(CACHE).then(x=>x.put(req,cp));}return r;}))); });
